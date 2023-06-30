5cb2df5615d52a7ded6b936370bc78377dd7e553
dafnemus <dafnemus@gmail.com>
2023-06-30 14:23:58 -0300

    Counter: - Created counter app, to sum or subtract

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,22 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const [start, setStart] = useState(false);
 
   return (
-    <>
+    <main>
       <h1>Counter:</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
-    </>
-  )
+      <button onClick={() => setStart(!start)}>count is {count}</button>
+      {start && (
+        <div className="btnContainer">
+          <button onClick={() => setCount((count) => count + 1)}>+</button>
+          <button onClick={() => setCount((count) => count - 1)}>-</button>
+        </div>
+      )}
+    </main>
+  );
 }
 
-export default App
+export default App;
